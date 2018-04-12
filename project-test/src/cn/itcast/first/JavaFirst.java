30d7c436b3ba078517cb58799c6aaa7dcd4ab6fd
BillLee <1170829346@qq.com>
2018-04-12 13:54:17 +0800

    第一次提交

---
+++ project-test/src/cn/itcast/first/JavaFirst.java
@@ -0,0 +1,14 @@
+package cn.itcast.first;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaFirst {
+
+	@Test
+	public void testGit() throws Exception {
+		System.out.println("测试而已");
+	}
+	
+}
